3d3fced89b19631f426ad80f26bd5538f094a6aa
Lucas Garron <code@garron.net>
2014-04-13 10:00:06 -0700

    Attempt to verify MAC in Python. Not working.

--- decrypt.py
+++ decrypt.py
@@ -17,9 +17,14 @@ iv = iv[0:13]
 
 ct = binascii.a2b_base64(ctJSON["ct"])
 print("[decrypt.py] IV length:", len(iv))
-print("[decrypt.py] hex:", iv.encode("hex"))
+print("[decrypt.py] IV:", iv.encode("hex"))
+
+tag_num_bytes = ctJSON["ts"] / 8
+print("[decrypt.py] Tag size (bytes):", tag_num_bytes)
+tag = ct[-tag_num_bytes:]
+print("[decrypt.py] Tag:", tag.encode("hex"))
 
 key = "\x00"*16
 cipher = AES.new(key, AES.MODE_CCM, iv[0:13])
-plaintext = cipher.decrypt(ct)
+plaintext = cipher.decrypt_and_verify(ct[:-tag_num_bytes], tag)
 print("[decrypt.py] Plaintext:", plaintext)
